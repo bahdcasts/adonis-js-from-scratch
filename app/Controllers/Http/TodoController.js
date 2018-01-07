d8d1be8646c7a3578d893673d8d34fa30ddd3c08
Bahdcoder <frantzenterprises777@gmail.com>
2018-01-07 05:03:39 +0100

    add indicative validation

--- app/Controllers/Http/TodoController.js
+++ app/Controllers/Http/TodoController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { validateAll } = use('Validator');
 const Todo = use('App/Models/Todo');
 
 class TodoController {
@@ -12,6 +13,22 @@ class TodoController {
 
   async store ({ request, response }) {
     const body = request.all();
+
+    // validate the data 
+    const rules = {
+      text: 'required|min:8'
+    };
+
+    const messages = {
+      'text.required': 'Please provide a todo.',
+      'text.min': 'Your todo is not long enough.'
+    };
+
+    const validator = await validateAll(body, rules, messages);
+    if (validator.fails()) {
+      console.log(validator.messages());
+      return response.redirect('/');
+    }
 
     const todo = await Todo.create({
       text: body.text
